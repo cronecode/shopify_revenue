553a0afec4565c76a53ad94cd3e9449df49c366b
cronecode <cronecode@gmail.com>
2017-01-07 18:44:44 -0500

    finished a working implementation

--- lib/shopify_revenue.ex
+++ lib/shopify_revenue.ex
@@ -5,8 +5,22 @@ defmodule ShopifyRevenue do
         HTTPoison.get!("https://shopicruit.myshopify.com/admin/orders.json?limit=250&fields=total_price&access_token=c32313df0d0ef512ca64d5b336a0d7c6")
         |> decode_json
         |> extract_list
-        |> IO.inspect
+        |> convert_price_strings
+        |> count_money
 
+    end
+
+    def count_money(prices) do
+        n = Enum.count(prices)
+        total = Enum.sum(prices)
+
+        rounded_total = Float.round(total, 2)
+
+        IO.puts "You've made $#{Float.to_string(rounded_total)} from #{Integer.to_string(n)} orders"
+    end
+
+    def convert_price_strings(list) do
+        for %{"total_price" => v} <- list, do: String.to_float(v)
     end
 
     def extract_list(map) do
